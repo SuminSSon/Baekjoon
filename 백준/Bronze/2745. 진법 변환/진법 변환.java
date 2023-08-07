92804e60273d651d7561f43dff7ee0e6e7d78b2a
SuminSSon <56192209+SuminSSon@users.noreply.github.com>
2023-08-07 11:32:00 +0900

    [Bronze II] Title: 진법 변환, Time: 216 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int num = sc.nextInt();
+        int temp = 1;
+        int result = 0;
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            char c = str.charAt(i);
+
+            if('A'<= c && c <= 'Z')
+                result += (c-'A'+10)*temp;
+            else
+                result += (c-'0')*temp;
+            temp *= num;
+        }
+        System.out.println(result);
+    }
+}
