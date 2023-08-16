b1e565e6959a31a1155d9df699132e53c7f791f8
SuminSSon <56192209+SuminSSon@users.noreply.github.com>
2023-08-16 15:30:30 +0900

    [Silver III] Title: 분수 합, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int gcd(int num1, int num2){
+        if(num2 == 0) return num1;
+        else return gcd(num2, num1 % num2);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int numerator = A * D + B*C;
+        int denominator = B * D;
+        int gcd = gcd(numerator, denominator);
+
+        sb.append(numerator/gcd).append(' ').append(denominator/gcd);
+        System.out.println(sb);
+    }
+}
