ab4edd1f362a054c148a6beb9a55f832b7d970cf
Sumin Son <56192209+SuminSSon@users.noreply.github.com>
2023-12-28 15:33:03 +0900

    [Silver IV] Title: 큐, Time: 200 ms, Memory: 19912 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        int last = 0;
+
+        while(N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push":
+                    last = Integer.valueOf(st.nextToken());
+                    queue.offer(last);
+                    break;
+                case "pop":
+                    if(queue.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(queue.poll()).append('\n');
+                    break;
+                case "size":
+                     sb.append(queue.size()).append('\n');
+                    break;
+                case "empty":
+                    if(queue.isEmpty())sb.append(1).append('\n');
+                    else sb.append(0).append('\n');
+                    break;
+                case "front":
+                    if(queue.isEmpty()) sb.append(-1).append('\n');
+                    else sb.append(queue.peek()).append('\n');
+                    break;
+                case "back":
+                    if(queue.isEmpty()) sb.append(-1).append("\n");
+                    else sb.append(last).append("\n");
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
+}
