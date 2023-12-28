bd079d0801905e1593dc4f807e1661315c4b5448
Sumin Son <56192209+SuminSSon@users.noreply.github.com>
2023-12-28 16:14:06 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 596 ms, Memory: 294812 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        LinkedList<Integer> queue = new LinkedList<>();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for(int i=1; i<=N; i++) queue.offer(i);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        while(queue.size() != 1){
+            for(int i=0; i < K - 1; i++) queue.offer(queue.poll());
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
+    }
+}
