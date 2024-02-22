ee48e4bb94fe04aaeb69b23e366cda9146b9d9ba
Sumin Son <56192209+SuminSSon@users.noreply.github.com>
2024-02-22 17:47:17 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 144 ms, Memory: 15796 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        String s = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
         int[] arr = new int[26];
-
-        for(int i=0; i<26; i++) arr[i] = -1;
-        for(int i=0; i<s.length(); i++){
-            int j = (int)s.charAt(i)-'a';
-            arr[j] = s.indexOf(s.charAt(i));
-        }
-        for(int index:arr) System.out.printf(index+" ");
+        for (int i = 0; i < 26; i++) arr[i] = -1;
+        for (int i = 0; i < s.length(); i++) arr[s.charAt(i) - 97] = s.indexOf(s.charAt(i));
+        for (int i = 0; i < 26; i++) System.out.print(arr[i]+" ");
     }
 }
