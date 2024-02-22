2551c480ba2dda2c3a0cafcf311e73066c80ad4c
Sumin Son <56192209+SuminSSon@users.noreply.github.com>
2024-02-22 17:48:50 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 140 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int[] alpa = new int[26];
+        for (int i = 0; i < s.length(); i++) alpa[s.charAt(i) - 97]++;
+        for (int i = 0; i < 26; i++) System.out.print(alpa[i] + " ");
+    }
+}
