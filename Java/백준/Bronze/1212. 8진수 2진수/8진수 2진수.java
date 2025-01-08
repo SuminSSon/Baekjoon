5eef9047fadc9de92f06806fd668580cdb564593
SuminSSon <56192209+SuminSSon@users.noreply.github.com>
2025-01-08 16:14:25 +0900

    [Bronze II] Title: 8진수 2진수, Time: 1964 ms, Memory: 84868 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+
+        BigInteger N = new BigInteger(S, 8);
+
+        String result = N.toString(2);
+
+        System.out.println(result);
+        br.close();
+    }
+}
